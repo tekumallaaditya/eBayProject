b968fc5f3898cd1bb6a746a21f46dd48bb3c7791
Aditya Tekumalla <tekumallaaditya@gmail.com>
2018-08-30 17:46:11 +0200

    more modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ var options = ['rock', 'paper', 'scissors'];
 function youVScomp(){
     playerName = $('#playerNameInput').val();    
     window.localStorage.setItem('name',playerName);    
-    window.location.href = '/youVScomp.html';  
+    window.location.href = 'youVScomp.html';  
 }
 
 //Thus function is executed when the load event happens in the youVScomp.html. this is used to send all the variables to html
@@ -24,7 +24,7 @@ function getName(){
 
 //This function is executed when rock button is clicked
 function rock(){
-    $('#option').attr('src', '/assets/images/rock.jpeg');
+    $('#option').attr('src', './assets/images/rock.jpeg');
     compChoose();
     playerMove = 'rock';
     winner();
@@ -35,8 +35,10 @@ function rock(){
     $('#roundLabel').show();
     roundCount();
 }
+
+//This function is executed when Paper button is clicked
 function paper(){
-    $('#option').attr('src', '/assets/images/paper.jpg');
+    $('#option').attr('src', './assets/images/paper.jpg');
     compChoose();
     playerMove = 'paper';
     winner();
@@ -47,8 +49,10 @@ function paper(){
     $('#roundLabel').show();
     roundCount();
 }
+
+//This function is executed when Scissors button is clicked
 function scissors(){
-    $('#option').attr('src', '/assets/images/scissors.jpg');
+    $('#option').attr('src', './assets/images/scissors.jpg');
     compChoose();
     playerMove = 'scissors';
     winner();
@@ -60,57 +64,71 @@ function scissors(){
     roundCount();
 }
 
+//this function randomly chooses for the comp
 function compChoose(){
    var ran_num =  Math.floor(Math.random() * Math.floor(3));
     var ran_choice = options[ran_num];
     console.log(ran_num, ran_choice);
     if (ran_num == 0){
-        $('#optionComp').attr('src', '/assets/images/rock.jpeg' );
+        $('#optionComp').attr('src', './assets/images/rock.jpeg' );
         compMove = 'rock';
         
     } else if (ran_num == 1){
-        $('#optionComp').attr('src', '/assets/images/paper.jpg' );
+        $('#optionComp').attr('src', './assets/images/paper.jpg' );
         compMove = 'paper';
     } else{
-        $('#optionComp').attr('src', '/assets/images/scissors.jpg' );
+        $('#optionComp').attr('src', './assets/images/scissors.jpg' );
         compMove = 'scissors';
     }   
     
 }
 
+//Every round this function is called and the winner is decided and counters increased
 function winner(){
     console.log('inside the winner function' + playerScore + compScore );
     if((playerMove == 'rock' && compMove == 'scissors') || (playerMove == 'paper' && compMove == 'rock') || (playerMove == 'scissors' && compMove == 'paper')){
         playerScore = playerScore + 1;
+        $('#roundWinner').show().html('<h4>You are the winner of this round</h4>');
     } else if((playerMove == 'rock' && compMove == 'rock') || (playerMove == 'paper' && compMove == 'paper') || (playerMove == 'scissors' && compMove == 'scissors')){
-        //do nothing
+        $('#roundWinner').show().html('<h4>it is a tie</h4>');
     } else if((playerMove == 'rock' && compMove == 'paper') || (playerMove == 'paper' && compMove == 'scissors') || (playerMove == 'scissors' && compMove == 'rock')){
         compScore = compScore +1;
+        $('#roundWinner').show().html('<h4>Comp are the winner of this round</h4>');
     }
 }
 
+//This function decides the final winner after all the ten rounds
 function roundCount(){
     console.log('inside the roundCount function');
     if(round == 10){
         console.log('inside the roundCount function');
         //$('#roundLabel').hide();
         if (playerScore > compScore){
+            $('#roundWinner').hide();
             var curName = window.localStorage.getItem('name');
-            $('#roundLabel').text( curName + ' is the winner');
+            $('#heading').hide();
+            $('#scoreboard').hide();
+            $('#winnerIMG').show().attr('src', './assets/images/winnerPlayer.gif' );
+            $('#roundLabel').html('<h3>' +curName + ' is the winner<h3>');
             $('button#rock').attr('disabled', 'disabled');
             $('button#paper').attr('disabled', 'disabled');
             $('button#scissors').attr('disabled', 'disabled');
             $('#replay').show();
             
         } else if(playerScore == compScore){
-            $('#roundLabel').text( 'Its a Tie');
+            $('#roundWinner').hide();
+            $('#roundLabel').html( '<h3>Its a Tie</h3>');
             $('button#rock').attr('disabled', 'disabled');
             $('button#paper').attr('disabled', 'disabled');
             $('button#scissors').attr('disabled', 'disabled');            
             $('#replay').show();
                   
         } else{
-            $('#roundLabel').text( 'Comp is the winner');
+            $('#roundWinner').hide();
+            $('#heading').hide();
+            $('#scoreboard').hide();
+            $('#winnerIMG').show().attr('src', './assets/images/loserPlayer.gif' );
+            $('#roundLabel').html( '<h3>YOU LOST!!!</h3>');
             $('button#rock').attr('disabled', 'disabled');
             $('button#paper').attr('disabled', 'disabled');
             $('button#scissors').attr('disabled', 'disabled');            
@@ -124,8 +142,18 @@ function roundCount(){
     }
 }
 
+
+//This onclick event is for redirecting to the home.html
 $(document).ready(function(){
     $('button#replay').on('click', function(){    
     window.location.href = 'home.html';
-})
-})
+});
+});
+
+//This onclick event is for redirecting to the compVScomp.html
+$(document).ready(function(){
+    $('#compVsComp').click(function(){
+    window.location.href = 'compVScomp.html';
+});
+});
+
---
+++ assets/js/second.js
@@ -0,0 +1,126 @@
+var counter= 0;
+var comp1Move;
+var comp2Move;
+var comp1Score = 0;
+var comp2Score = 0;
+var round = 0;
+var options = ['rock', 'paper', 'scissors'];
+
+//This function is called on the event of onload and enables the start button
+function needRight(){
+    $('#startGame').show();
+}
+
+//This onclick event hides the start button and shows the round information
+$(document).ready(function(){
+    $('#startGame').click(function(){
+        $('#startGame').hide();
+        $('#roundLabel').show();
+        startGameProcess();
+    });
+});
+
+//This function takes care of automatically calling the random calls for comp1 and comp2 and ending after 10 rounds
+function startGameProcess(){
+    var gameIntervalId = setInterval(function(){
+        round += 1;
+        comp1Choose();
+        comp2Choose();
+        winner();
+        $('#compScore1').text(comp1Score);
+        $('#compScore2').text(comp2Score);
+        $('#roundNumber').text(round);
+        
+        console.log('comp1 score is -> '+ comp1Score + 'comp2 score is -> '+ comp2Score );
+        
+        if(round === 10){
+            console.log('inside the clearInterval function');                      
+            clearInterval(gameIntervalId);
+            roundCount();
+        }
+        
+    }, 2000);
+    
+    
+}
+
+//This function is called after every round to decide the winner of that round
+function winner(){
+    console.log('inside the winner function' + comp1Score + comp2Score );
+    if((comp1Move == 'rock' && comp2Move == 'scissors') || (comp1Move == 'paper' && comp2Move == 'rock') || (comp1Move == 'scissors' && comp2Move == 'paper')){
+        comp1Score = comp1Score + 1;
+        $('#roundWinner').show().html('<h4>Comp 1 is the winner of this round</h4>');
+    } else if((comp1Move == 'rock' && comp2Move == 'rock') || (comp1Move == 'paper' && comp2Move == 'paper') || (comp1Move == 'scissors' && comp2Move == 'scissors')){
+        $('#roundWinner').show().html('<h4>It is a tie</h4>');
+    } else if((comp1Move == 'rock' && comp2Move == 'paper') || (comp1Move == 'paper' && comp2Move == 'scissors') || (comp1Move == 'scissors' && comp2Move == 'rock')){
+        comp2Score = comp2Score +1;
+        $('#roundWinner').show().html('<h4>Comp 2 is the winner of this round</h4>');
+    }
+}
+
+//This is the random choice of comp1
+function comp1Choose(){
+   var ran_num =  Math.floor(Math.random() * Math.floor(3));
+    var ran_choice = options[ran_num];
+    console.log(ran_num, ran_choice);
+    if (ran_num == 0){
+        $('#option').attr('src', './assets/images/rock.jpeg' );
+        comp1Move = 'rock';
+        
+    } else if (ran_num == 1){
+        $('#option').attr('src', './assets/images/paper.jpg' );
+        comp1Move = 'paper';
+    } else{
+        $('#option').attr('src', './assets/images/scissors.jpg' );
+        comp1Move = 'scissors';
+    }   
+    
+}
+
+//This is the random choice to comp2
+function comp2Choose(){
+   var ran_num =  Math.floor(Math.random() * Math.floor(3));
+    var ran_choice = options[ran_num];
+    console.log(ran_num, ran_choice);
+    if (ran_num == 0){
+        $('#optionComp').attr('src', './assets/images/rock.jpeg' );
+        comp2Move = 'rock';
+        
+    } else if (ran_num == 1){
+        $('#optionComp').attr('src', './assets/images/paper.jpg' );
+        comp2Move = 'paper';
+    } else{
+        $('#optionComp').attr('src', './assets/images/scissors.jpg' );
+        comp2Move = 'scissors';
+    }   
+    
+}
+
+//This function is called after all ten rounds are done and final winner is to be decided
+function roundCount(){
+    console.log('inside the roundCount function');
+    if(round == 10){
+        console.log('inside the roundCount function');
+        //$('#roundLabel').hide();
+        if (comp1Score > comp2Score){
+            $('#roundWinner').hide();
+            $('#roundLabel').html( '<h3>Comp 1 is the final winner</h3>');           
+            $('#replay').show();
+            
+        } else if(comp1Score == comp2Score){
+            $('#roundWinner').hide();
+            $('#roundLabel').html( '<h3>The Game ends in a tie</h3>');                       
+            $('#replay').show();
+                  
+        } else{
+            $('#roundWinner').hide();
+            $('#roundLabel').html( '<h3>Comp 2 is the final winner</h3>');                      
+            $('#replay').show();
+        }       
+        
+         comp1Score = 0;
+         comp2Score = 0;
+         round = 0;
+        
+    }
+}
